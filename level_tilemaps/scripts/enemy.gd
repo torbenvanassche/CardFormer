37ddf78d62c3c83abf8df4704706a12ec8d8f78b
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-07-07 23:20:04 +0200

    fixed a small bug causing an infinite loop

--- level_tilemaps/scripts/enemy.gd
+++ level_tilemaps/scripts/enemy.gd
@@ -2,9 +2,8 @@ class_name Enemy
 extends Area2D
 
 func on_enter():
-	var scene_init_data = {
-		"player": Platformer.instance.player,
-		"enemy": self
-	}
-	
-	SceneManager.instance.set_active_scene("battle", SceneConfig.new(true, true, false, false, scene_init_data))
+	if get_parent() is Platformer:
+		_on_enter_platformer();
+
+func _on_enter_platformer():
+	SceneManager.instance.set_active_scene("battle", SceneConfig.new(true, true, false, false, { "player": Platformer.instance.player, "enemy": self }))
